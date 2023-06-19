e6fa052d57288b014fefa80756656fda38e9b476
Vyrmedea <noilndygs@gmail.com>
2023-06-19 17:59:38 +0800

    1

--- SystemNuke/EventHandlers.cs
+++ SystemNuke/EventHandlers.cs
@@ -19,6 +19,7 @@
                 Exiled.API.Features.Broadcast broadcast =new Exiled.API.Features.Broadcast($"SITE-02已被放弃，系统核弹将于{Warhead.RealDetonationTimer}秒后爆炸");
                 foreach ( Player p in Player.List)
                 {
+                    if (p == null) continue;
                     p.Broadcast( broadcast );
                 }
             }
@@ -32,6 +33,7 @@
                 Exiled.API.Features.Broadcast broadcast=new Exiled.API.Features.Broadcast($"为保证安全，基金会已发射核弹，将于{i}秒后到达", 1);
                 foreach (Player p in Player.List)
                 {
+                    if (p == null) continue;
                     p.Broadcast(broadcast);
                 }
             }
@@ -39,6 +41,7 @@
             {
                 if (p.IsAlive)
                 {
+                    if (p == null) continue;
                     p.Kill(DamageType.Warhead);
                 }
             }
